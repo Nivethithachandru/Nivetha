c59cdcc34d535269af4bdd97f949ec5fe2518717
Nivethithachandrakrishnan <141031571+Nivethithachandru@users.noreply.github.com>
2025-09-16 11:10:19 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -135,9 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [STATIC_DIR,]
-STATIC_ROOT = os.path.join(BASE_DIR ,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MEDIA_URL = '/images/'
